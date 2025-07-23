b884610eb597412cd5a101e83b519c087c728d80
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-07-23 22:44:33 +0500

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+# Using nested if statements
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is greater than 20.")
+else:
+    print("Your age is less than 10.")
